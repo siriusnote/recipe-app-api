6dc152cb1c550b7a28d4e3ab68cd1c12dfc5b2f8
Sirius Tam <sirius731@gmail.com>
2023-02-16 13:02:16 +0800

    Fixed Flake8 warning

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,10 +1,28 @@
 """
 Django command to wait for the database to be available
 """
+import time
+
+from pymysql import OperationalError as MysqlOpError
+
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """Django command to wait for database."""
 
     def handle(self, *args, **options):
-        pass
+        """Entrypoint for command."""
+        self.stdout.write("Waiting for database...")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except (MysqlOpError, OperationalError) as e:
+                self.stdout.write(str(e))
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
