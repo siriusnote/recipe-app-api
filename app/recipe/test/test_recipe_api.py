fe8b009e74a09a3f590cfad99fe622587fcaad32
Sirius Tam <sirius731@gmail.com>
2023-02-20 11:43:17 +0800

    Fixed Syntax

--- app/recipe/test/test_recipe_api.py
+++ app/recipe/test/test_recipe_api.py
@@ -196,5 +196,3 @@ class PrivateRecipeAPITests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
         self.assertTrue(Recipe.objects.filter(id=recipe.id).exists())
-
-
